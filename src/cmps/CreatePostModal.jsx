54f426abca6ae7f531880193a8fec7cfc6c6e222
Gal Luski <luskigal@gmail.com>
2023-10-06 16:16:09 +0300

    F/ create modal css

--- src/cmps/CreatePostModal.jsx
+++ src/cmps/CreatePostModal.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
-import createSvg from '/src/assets/icons/create.svg'
+import createModalSvg from '/src/assets/icons/create-modal.svg'
+import backSvg from '/src/assets/icons/back.svg'
 import { uploadService } from "../services/upload.service";
 import { systemReducer, LOADING_DONE, LOADING_START } from "../store/system.reducer";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,7 +9,7 @@ export function CreatePostModal({ onCloseModal, onAddPost }) {
     const dispath = useDispatch()
     const isLoading = useSelector(storeState => storeState.systemModule.isLoading)
     const [selectedFile, setSelectedFile] = useState(null);
-    
+
 
     async function handleAddPhoto(ev) {
         dispath({
@@ -29,36 +30,40 @@ export function CreatePostModal({ onCloseModal, onAddPost }) {
     if (isLoading) {
         return (
             <section className="modal">
-                <div >
-                    <h2>Uploading photo...</h2>
+                <div className="modal-header">
+                    <h4>Create new post</h4>
+                    <hr />
                 </div>
-
+                <div class="loader-line"></div>
             </section>
         )
     }
     if (selectedFile) {
 
         return (
-            <section className="modal">
-                <div >
-                    <button onClick={() => setSelectedFile(null)} >another</button>
-                    <button onClick={() => onAddPost('this shuld be input', selectedFile)}>Share</button>
-
+            <section className="modal-edit">
+                <div className="modal-header-share" >
+                    <button className="btn-back" onClick={() => setSelectedFile(null)} ><img src={backSvg} alt="back" /></button>
+                    <p><strong>Create new post</strong></p>
+                    <button className="btn-share" onClick={() => onAddPost('this should be input', selectedFile)}>Share</button>
                 </div>
+                <div className="modal-edit-area">
                 <img style={{ width: '100%', objectFit: 'fill', height: '100%' }} src={selectedFile} alt="Uploaded" />
+                <textarea rows="2" cols="50" placeholder="Write a caption..."></textarea>
+                </div>
                 <button onClick={onCloseModal} className="close-modal">X</button>
-
             </section>
         )
     } else return (
         <section className="modal">
             <div className="modal-header">
-
                 <h4>Create new post</h4>
+                <hr />
             </div>
             <div className="add-files">
-                <img style={{ width: '33%', margin: '5px auto' }} src={createSvg} alt="" />
-                <input onChange={handleAddPhoto} type="file" />
+                <img style={{ width: '150px', margin: '5px auto' }} src={createModalSvg} alt="" />
+                <label htmlFor="btn-upload" className="label-upload">Select from computer</label>
+                <input id="btn-upload" className="btn-upload" onChange={handleAddPhoto} type="file" />
                 <button onClick={onCloseModal} className="close-modal">X</button>
             </div>
         </section>
